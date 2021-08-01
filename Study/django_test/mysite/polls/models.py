7cb3f219dcce2608d82bc097f2c23d46de5e42cc
eogns0321 <eogns0321@gmail.com>
2021-08-01 03:53:25 -0400

    django polls project complete(somewhat)

--- Study/django_test/mysite/polls/models.py
+++ Study/django_test/mysite/polls/models.py
@@ -15,9 +15,12 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE);
+    question = models.ForeignKey(Question, on_delete=models.CASCADE);
     choice_text = models.CharField(max_length=200);
-    votes = models.IntegerField(default = 0);
+    votes = models.IntegerField(default=0);
 
     def __str__(self):
         return self.choice_text;
+
+
+    
