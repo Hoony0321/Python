37c3d2fd28c251b6e0278f9727264e2a9308b389
eogns0321 <eogns0321@gmail.com>
2021-08-05 09:22:09 -0400

    Apply git ignore

--- django_lecture/pragmatic/pragmatic/settings.py
+++ django_lecture/pragmatic/pragmatic/settings.py
@@ -12,15 +12,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os,environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# reading .env file
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR,'.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#twkuaq9mvqczqf^)h4qlmvy52$r(vdpu^5#4wk&k953(4enn$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
