0445946147c02c0a98c35924c848f4472e1d7390
eogns0321 <eogns0321@gmail.com>
2021-08-06 07:16:19 -0400

    complete django course 11

--- django_lecture/pragmatic/pragmatic/settings.py
+++ django_lecture/pragmatic/pragmatic/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
