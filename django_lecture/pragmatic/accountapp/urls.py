ea30f94f9e3961596a3a78bc49b0e703a928778c
eogns0321 <eogns0321@gmail.com>
2021-08-06 23:20:31 -0400

    djnago course 16

--- django_lecture/pragmatic/accountapp/urls.py
+++ django_lecture/pragmatic/accountapp/urls.py
@@ -5,5 +5,5 @@ app_name = 'accountapp'
 # 이미 이 urls.py 분기로 들어올때 account/ 를 쓰는데 왜 또 app_name을 명시하는가?
 # 나중에 accountapp만 써도 바로 갈 수 있는 함수를 설정하기 위해!
 urlpatterns = [
-    path('hello_world', views.hello_world, name='hello_world'),
+    path('hello_world/', views.hello_world, name='hello_world'),
 ]
--- django_lecture/pragmatic/accountapp/views.py
+++ django_lecture/pragmatic/accountapp/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request): #인자로 request가 들어가는 이유는 client에게 요청을 받아 처리하기 때문!
-    return render(request, 'accountapp/hello_world.html');
+    
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html' , context ={ 'text' : "POST METHOD"});
+    else :
+        return render(request, 'accountapp/hello_world.html');
