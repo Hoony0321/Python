d2e8fe4ec47d323bc11be48ea032ac2f55435705
eogns0321 <eogns0321@gmail.com>
2021-08-09 07:25:44 -0400

    django course 27

--- django_lecture/pragmatic/accountapp/views.py
+++ django_lecture/pragmatic/accountapp/views.py
@@ -9,22 +9,26 @@ from .forms import AccountUpdateForm
 
 def hello_world(request): #인자로 request가 들어가는 이유는 client에게 요청을 받아 처리하기 때문!
     
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input');
-        
-        new_hello_world = HelloWorld();
-        new_hello_world.text = temp;
-        new_hello_world.save();
+            temp = request.POST.get('hello_world_input');
+            
+            new_hello_world = HelloWorld();
+            new_hello_world.text = temp;
+            new_hello_world.save();
 
-        hello_world_list = HelloWorld.objects.all();
+            hello_world_list = HelloWorld.objects.all();
+            
+            #F5 새로고침 후 계속 POST 방식의 홈페이지가 생성되지 않도록 redirect로 GET 방식의 홈페이지로 돌아감.
+            return HttpResponseRedirect(reverse('accountapp:hello_world')); 
         
-        #F5 새로고침 후 계속 POST 방식의 홈페이지가 생성되지 않도록 redirect로 GET 방식의 홈페이지로 돌아감.
-        return HttpResponseRedirect(reverse('accountapp:hello_world')); 
-    
-    else :
-        hello_world_list = HelloWorld.objects.all();
-        return render(request, 'accountapp/hello_world.html' , context ={ 'hello_world_list' : hello_world_list });
+        else :
+            hello_world_list = HelloWorld.objects.all();
+            return render(request, 'accountapp/hello_world.html' , context ={ 'hello_world_list' : hello_world_list });
+        
+    else:
+        return HttpResponseRedirect(reverse('accountapp:login')); 
 
 
 class AccountCreateView(generic.CreateView):
