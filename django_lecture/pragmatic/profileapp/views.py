a90f5de19bc0328ec85a79bda1a30b50a78f89e6
eogns0321 <eogns0321@gmail.com>
2021-08-09 10:33:35 -0400

    django course 31

--- django_lecture/pragmatic/profileapp/views.py
+++ django_lecture/pragmatic/profileapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from profileapp.models import Profile
+from profileapp.forms import ProfileCreationForm
+from django.urls import reverse_lazy
 # Create your views here.
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        
+        return super().form_valid(form)
