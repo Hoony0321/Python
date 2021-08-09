1cec3d57469c4dd0e4f274a238ed4e3b04fa721f
eogns0321 <eogns0321@gmail.com>
2021-08-09 09:45:42 -0400

    django course 30

--- django_lecture/pragmatic/pragmatic/settings.py
+++ django_lecture/pragmatic/pragmatic/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accountapp',
     'bootstrap4',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
---
+++ django_lecture/pragmatic/profileapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(max_length=100, null=True)
