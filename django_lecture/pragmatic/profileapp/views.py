7e402c373f3480ecb1e759be1257f4ed21e074cb
eogns0321 <eogns0321@gmail.com>
2021-08-10 10:11:51 -0400

    django course 32

--- django_lecture/pragmatic/profileapp/views.py
+++ django_lecture/pragmatic/profileapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from profileapp.models import Profile
 from profileapp.forms import ProfileCreationForm
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from profileapp.decorators import profile_ownership_required
 # Create your views here.
 
 class ProfileCreateView(CreateView):
@@ -18,3 +20,12 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         
         return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
