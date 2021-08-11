c48d25ef57261abb5a087f3146a4fea99c938650
eogns0321 <eogns0321@gmail.com>
2021-08-11 10:09:04 -0400

    django course 34

--- django_lecture/pragmatic/pragmatic/settings.py
+++ django_lecture/pragmatic/pragmatic/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'accountapp',
     'bootstrap4',
     'profileapp',
+    'articleapp',
 ]
 
 MIDDLEWARE = [
--- django_lecture/pragmatic/pragmatic/urls.py
+++ django_lecture/pragmatic/pragmatic/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
