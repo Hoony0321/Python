bee4182193158e970f7fb18215c1ed98053abce9
eogns0321 <eogns0321@gmail.com>
2021-08-11 09:37:30 -0400

    django course 33

--- django_lecture/pragmatic/profileapp/views.py
+++ django_lecture/pragmatic/profileapp/views.py
@@ -11,7 +11,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -21,11 +20,16 @@ class ProfileCreateView(CreateView):
         
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk' : self.object.user.pk})
+
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accountapp:detail', kwargs={'pk' : self.object.user.pk})
