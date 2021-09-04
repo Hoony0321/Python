04aeee0efe674766c8811231716f17e24e467bed
eogns0321 <eogns0321@gmail.com>
2021-09-04 10:58:18 -0400

    2021_09_04 start currency detail html

--- django_lecture/practice/coinapp/urls.py
+++ django_lecture/practice/coinapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 
-from .views import home
+from .views import home, DetailView
 
 app_name="coinapp"
 urlpatterns = [
     path("home/", home, name="home"),
+    path("detail/", DetailView, name="detail")
 ]
--- django_lecture/practice/coinapp/views.py
+++ django_lecture/practice/coinapp/views.py
@@ -12,9 +12,17 @@ def home(request):
     allInfo = pybit.get_current_price("ALL")
 
     number_of_object_in_page = 20
-    paginator = Paginator(tuple(allInfo), number_of_object_in_page)
-    page = request.GET.get('page')
+    paginator = Paginator(tuple(allInfo.items()), number_of_object_in_page)
+    page = request.GET.get('page' , 1)
     post = paginator.get_page(page)
     
-    return render(request,'coinapp/home.html', {'post' : post , 'number' : number_of_object_in_page})
+    return render(request,'coinapp/home.html', {'post' : post } )
+
+def DetailView(request):
+    
+    currency = request.GET.get('currency', 'BTC')
+
+    info = pybit.get_current_price("BTC")
+
+    return render(request, 'coinapp/detail.html', {'currency' : currency})
 
